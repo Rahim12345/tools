5bc0edb2e5f361365195a2eb0b65124482be71be
Rahim Süleymanov <rahim.suleymanov94@gmail.com>
2020-10-08 21:26:26 +0400

    Create 1050.php

---
+++ laravel/errors/1050.php
@@ -0,0 +1,15 @@
+<?php
+/*
+
+SQLSTATE[42S01]: Base table or view already exists: 1050 Table 'users' already exists 
+
+
+
+1: Inside Folder you will see App Go to this folder
+2: Now you will have multiple folders look for " Providers "
+3: Inside this folder you will get File called " AppServiceProvider.php "
+4: Open this file and add this line " Schema::defaultStringLength(191); " inside function called public f
+function boot() { Here paste it }
+5: Save the file
+
+*/
