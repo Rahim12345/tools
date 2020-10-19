138af3f5a76a3c7cfebb98bb3ff792c5e06aff6b
Rahim Süleymanov <rahim.suleymanov94@gmail.com>
2020-10-19 17:02:12 +0400

    Update profileImage.php

--- laravel/auth/profileImage.php
+++ laravel/auth/profileImage.php
@@ -2,4 +2,15 @@
 /**
  * 1. .env file da APP_URL=http://127.0.0.1:8000 edirik
  * 2. terminalda php artisan storage:link calisdirmaq
+ * _____________________________________________________________
+ * //Get file with extension
+ *  $filenameWithExt = $input['photo']->getClientOriginalName();
+ * //Get just filename
+ *  $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+ * //Get just ext
+ *  $extension = $input['photo']->getClientOriginalExtension();
+ * //Filename to store
+ *  $fileNameToStore = $filename.'_'.time().'.'.$extension;
+ * //Upload Image
+ *  $path = $input['photo']->storeAs('public/profile-photos', $fileNameToStore);
  */
