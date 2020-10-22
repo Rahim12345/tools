d6c60a259703d48681d86fbe998c3f2d3f751337
Rahim Süleymanov <rahim.suleymanov94@gmail.com>
2020-10-22 14:41:13 +0400

    Create validator_extend.php

---
+++ laravel/validation/validator_extend.php
@@ -0,0 +1,14 @@
+<?php
+//1) bunu uyğun funksiyada elave edirik
+//2)Controllerdə və ya App\Providers\AppServiceProvider.php də 
+/**
+ *      public function boot()
+ *      {
+ *          Validator::extend('alphaspace', function($attribute, $value, $parameters)
+ *          {
+ *              return preg_match('/^[\pL\s]+$/u', $value);
+ *          });
+ *      }
+ */
+ 
+
