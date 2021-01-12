cbbbeaa3af7c2a447d1f62356155d313586843cf
Rahim Süleymanov <rahim.suleymanov94@gmail.com>
2021-01-13 03:39:22 +0400

    User agent

--- laravel/user agent/userDetails.php
+++ laravel/user agent/userDetails.php
@@ -1,12 +1,12 @@
 <?php
-
+//IP almaq==============>
 // PHP program to get IP address of client 
 $IP = $_SERVER['REMOTE_ADDR']; 
   
 // $IP stores the ip address of client 
 echo "Client's IP address is: $IP"; 
 
-
+//MAC almaq==============>
 // PHP code to get the MAC address of Client 
 $MAC = exec('getmac'); 
   
@@ -19,3 +19,39 @@ $MAC = strtok($MAC, ' ');
 // because getmac returns transport name after 
 // MAC address    
 echo "MAC address of client is: $MAC"; 
+
+//ISP almaq==============>
+$json=file_get_contents("https://extreme-ip-lookup.com/json/$ip");
+extract(json_decode($json,true));
+echo "ISP: $isp ($city, $region, $country)<br>";
+
+
+//Daha cox ==============>
+function getIpInfo($ip = '') {
+    $ipinfo = file_get_contents("https://ipinfo.io/212.47.141.95");
+    $ipinfo_json = json_decode($ipinfo, true);
+    return $ipinfo_json;
+}
+
+function displayIpInfo($ipinfo_json) {
+    var_dump($ipinfo_json);
+    echo <<<END
+<pre>
+ip      : {$ipinfo_json['ip']}
+city    : {$ipinfo_json['city']}
+region  : {$ipinfo_json['region']}
+country : {$ipinfo_json['country']}
+loc     : {$ipinfo_json['loc']}
+postal  : {$ipinfo_json['postal']}
+org     : {$ipinfo_json['org']}
+</pre>
+END;
+}
+
+function main() {
+    $ipinfo_json = getIpInfo();
+
+    print_r($ipinfo_json);
+}
+
+main();
