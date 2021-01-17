7af0e1686b92b65c42ab2bee3e070ccab60763f2
Rahim Süleymanov <rahim.suleymanov94@gmail.com>
2021-01-17 23:12:53 +0400

    Update symlink.php

--- laravel/symlink.php
+++ laravel/symlink.php
@@ -2,11 +2,15 @@ Projects 	->Main project
           ->Sub project 1
           ->Sub project 2 …
 
-public_html	->(move here all public files of Main project,index.php edit,create storage folder and add 
+public_html    ->(move here all public files of Main project,index.php edit,create storage folder and add 
                ln -s /home/u163760621/domains/rahimsuleymanov.com/Projects/myPortfolio/storage/app/public/* /home/u163760621/domains/rahimsuleymanov.com/public_html/storage 	
                )
-      ->sub1	->index.php
-        ->.htaccess
-      ->sub2	->index.php
-        ->.htaccess
-      …
+
+                ->sub1	->(move here all public files of Sub project 1,index.php edit,create storage folder and add 
+                                   ln -s /home/u163760621/domains/rahimsuleymanov.com/Projects/Sub_project_2/storage/app/public/* /home/u163760621/domains/rahimsuleymanov.com/public_html/sub1/storage 	
+                                   )
+
+                ->sub2	->(move here all public files of Sub project 2,index.php edit,create storage folder and add 
+                                   ln -s /home/u163760621/domains/rahimsuleymanov.com/Projects/Sub_project_2/storage/app/public/* /home/u163760621/domains/rahimsuleymanov.com/public_html/sub2/storage 	
+                                   )
+                …
